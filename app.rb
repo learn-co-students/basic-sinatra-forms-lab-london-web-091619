4d148a2eefbc8383d46031c94997c28007a50a3e
Michael Williamson <michaelrwilliamson92@gmail.com>
2019-10-09 11:10:10 +0100

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    post '/team' do
+       
+       @id = params[:id]
+        @name = params[:name]
+        @coach = params[:coach]
+        @pg = params[:pg]
+        @sg = params[:sg]
+        @sf = params[:sf]
+        @pf = params[:pf]
+        @c = params[:c]
+       
+       erb :team
+    end
+
+    get '/newteam' do
+        erb :newteam
+    end
+
 
 end
